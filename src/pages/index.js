a0262aaef367f860c63a574983869d38efe3fd2f
gabriel2302 <gabriel23002@gmail.com>
2021-02-05 19:09:05 -0300

    Configuração do tema

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <>
+      <div>Clone youtube com next js</div>
+      <Button color="primary" variant="contained">
+        Botão
+      </Button>
+    </>
+  );
+}
---
+++ src/theme/index.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { colors } from '@material-ui/core';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: colors.common.white,
+      dark: '#f4f6f8',
+      paper: colors.common.white,
+    },
+    primary: {
+      main: '#f44336',
+    },
+    secondary: {
+      main: '#3EA6FF',
+    },
+  },
+});
+
+export default theme;
