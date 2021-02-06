5a66cdb5ca962211ccb4284ff96e2b98cccd6c4d
gabriel2302 <gabriel23002@gmail.com>
2021-02-05 21:10:25 -0300

    Caminho absoluto na raiz

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+import Layout from 'src/components/Layout';
 
 export default function Home() {
   return (
