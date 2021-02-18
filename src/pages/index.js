18b18c852d92441d5471932aa7322338b05c10b2
gabriel2302 <gabriel23002@gmail.com>
2021-02-18 17:41:33 -0300

    VideoCard Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
 import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="YouTube">
-      <Button color="primary" variant="contained">
-        Botão
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item x1={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'https://miro.medium.com/max/2800/1*qaQYJcS48yV8oNJO8u8L5w.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
